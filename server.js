aae9a02532ba9c0605ebf6617c889f7cc875517b
Thomas Smith <thomas.1993.smith@gmail.com>
2016-04-13 10:51:03 +1000

    Add POST for todos

--- server.js
+++ server.js
@@ -1,20 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [];
+var todoNextId = 1;
 
-var todos = [{
-	id: 1,
-	description: "Meet Mum for lunch",
-	completed: false
-},{
-	id: 2,
-	description: "Go to market",
-	completed: false
-},{
-	id:3,
-	description: "Go to Nicoles",
-	completed: true
-}];
+app.use(bodyParser.json());
 
 app.get('/',  function(req, res){
 	res.send('Todo API root');
@@ -43,6 +34,23 @@ app.get('/todos/:id', function(req, res){
 	}else {
 		res.status(404).send();
 	}
+
+});
+
+// POST /todos 
+
+app.post('/todos', function(req, res){
+	var body = req.body;
+
+	// add id field
+	body.id = todoNextId++;
+
+	//push body into array 
+	todos.push(body);
+
+
+
+	res.json(body);
 
 });
 

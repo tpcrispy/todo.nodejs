f7e50e97eb3e6f6aa55ab31dd9f3f368645eb68c
Thomas Smith <thomas.1993.smith@gmail.com>
2016-04-15 13:45:54 +1000

    Convert GET /todos/:id to use sequelize

---
+++ models/todo.js
@@ -0,0 +1,21 @@
+module.exports = function(sequelize, DataTypes) {
+
+	return sequelize.define('todo', {
+		description: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				len: [1, 200]
+
+			}
+
+		},
+		completed: {
+			type: DataTypes.BOOLEAN,
+			allowNull: false,
+			defaultValue: false
+
+		}
+
+	});
+};
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -44,32 +45,33 @@ app.get('/todos', function(req, res) {
 // GET /todos/:id
 app.get('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {
-		id: todoId
-	});
 
-	if (matchedTodo) {
-		{}
-		res.json(matchedTodo);
-	} else {
-		res.status(404).send();
-	}
+	db.todo.findById(todoId).then(function(todo) {
+		if (!!todo) {
+			res.json(todo.toJSON());
+		} else {
+			res.status(404).send();
+		}
+
+	}, function(e) {
+		res.status(400).json(e);
+
+	});
 });
 
 // POST /todos
 app.post('/todos', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 
-	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-		return res.status(400).send();
-	}
+	db.todo.create(body).then(function(todo) {
+		res.json(todo.toJSON())
 
-	body.description = body.description.trim();
-	body.id = todoNextId++;
+	}, function(e) {
+		res.status(400).json(e);
 
-	todos.push(body);
+	});
 
-	res.json(body);
+
 });
 
 // DELETE /todos/:id
@@ -119,6 +121,9 @@ app.put('/todos/:id', function(req, res) {
 	res.json(matchedTodo);
 });
 
-app.listen(PORT, function() {
-	console.log('Express listening on port ' + PORT + '!');
-});
+db.sequelize.sync().then(function() {
+	app.listen(PORT, function() {
+		console.log('Express listening on port ' + PORT + '!');
+	});
+
+})

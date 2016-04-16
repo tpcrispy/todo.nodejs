890e7bffc3b545dc6cf64e52fb5861a056d775a7
Thomas Smith <thomas.1993.smith@gmail.com>
2016-04-16 18:24:47 +1000

    Convert GET/todos to sequelize

--- server.js
+++ server.js
@@ -16,30 +16,33 @@ app.get('/', function(req, res) {
 
 // GET /todos
 app.get('/todos', function(req, res) {
-	var queryParams = req.query;
-	var filteredTodos = todos;
+	var query = req.query;
+	var where = {};
 
-	// if has property && completed === 'true'
-	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-		filteredTodos = _.where(filteredTodos, {
-			completed: true
-		});
+	if (query.hasOwnProperty('completed') && query.completed === 'true'){
+		where.completed = true;
 
-	} else if (queryParams.hasOwnProperty('false') && queryParams.false === 'false') {
-		filteredTodos = _.where(filteredTodos, {
-			completed: false
-		});
+		}else if (query.hasOwnProperty('completed') && query.completed === 'false'){
+			where.completed = false;
+		}
+
+
+	if (query.hasOwnProperty('q') && query.q.length > 0) {
+		where.description = {
+			$like: '%' + query.q + '%'
+		};
+
+
 	}
 
-	//Search feature 
-	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-		filteredTodos = _.filter(filteredTodos, function(todo) {
-			return todo.description.toLowerCase().indexOf(queryParams.q) > -1;
+	db.todo.findAll({where: where}).then(function(todos){
+		res.json(todos);
+	}, function (e){
+		res.status(500).send();
 
-		});
-	}
+	});
 
-	res.json(filteredTodos);
+
 });
 
 // GET /todos/:id

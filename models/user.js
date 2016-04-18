064d0f6bb3cebb5ecac6de7de7bb1171b1f73890
Thomas Smith <thomas.1993.smith@gmail.com>
2016-04-18 20:58:24 +1000

    Convert email to lowercase via hook

--- models/user.js
+++ models/user.js
@@ -14,6 +14,14 @@ module.exports = function(sequelize, DataTypes) {
 				len: [6, 100]
 			}
 		}
+	}, {
+		hooks: {
+			beforeValidate: function (user, options){
+				if (typeof user.email === 'string') {
+					user.email = user.email.toLowerCase();
+				}
+			}
+		}
 	});
 
 };

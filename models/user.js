f200a4475b2c274080718f668aa22c0867dbbc16
Thomas Smith <thomas.1993.smith@gmail.com>
2016-04-19 11:46:56 +1000

    Add login route and authenticate class method

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ var bcrypt = require('bcryptjs');
 var _ = require('underscore');
 
 module.exports = function(sequelize, DataTypes) {
-	return sequelize.define('user', {
+	var user = sequelize.define('user', {
 		email: {
 			type: DataTypes.STRING,
 			allowNull: false,
@@ -25,11 +25,11 @@ module.exports = function(sequelize, DataTypes) {
 			validate: {
 				len: [6, 100]
 			},
-			set: function (value){
+			set: function(value) {
 				var salt = bcrypt.genSaltSync(10);
 				var hashedPassword = bcrypt.hashSync(value, salt);
 
-				this.setDataValue('password' , value);
+				this.setDataValue('password', value);
 				this.setDataValue('salt', salt);
 				this.setDataValue('password_hash', hashedPassword);
 			}
@@ -38,16 +38,43 @@ module.exports = function(sequelize, DataTypes) {
 		hooks: {
 			beforeValidate: function(user, options) {
 				if (typeof user.email === 'string') {
-					user.email = user.email.toLowerCase();
+				
 				}
 			}
 		},
-		instanceMethods:  {
-			toPublicJSON: function () {
+		classMethods: {
+			authenticate: function(body) {
+				return new Promise(function(resolve, reject) {
+	
+					if (typeof body.email !== 'string' || typeof body.password !== 'string') {
+						return reject();
+					}
+
+					user.findOne({
+						where: {
+							email: body.email
+						}
+					}).then(function(user) {
+						if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
+							return reject();
+						}
+
+						resolve(user);
+					}, function(e) {
+
+						reject();
+					});
+				});
+			}
+
+		},
+		instanceMethods: {
+			toPublicJSON: function() {
 				var json = this.toJSON();
 				return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
 			}
 		}
 	});
 
+	return user;
 };
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
+var bcrypt = require('bcryptjs');
 
 
 var app = express();
@@ -140,23 +141,37 @@ app.put('/todos/:id', function(req, res) {
 
 
 //POST /USERS
-app.post('/users', function (req, res){
+app.post('/users', function(req, res) {
 	var body = _.pick(req.body, 'email', 'password');
 
-	db.user.create(body).then(function (user){
+	db.user.create(body).then(function(user) {
 		res.json(user.toPublicJSON());
-	}, function (e){
+	}, function(e) {
 		res.status(400).json(e);
 
 	});
 
 });
 
+// POST /users/login
+app.post('/users/login', function(req, res) {
+	var body = _.pick(req.body, 'email', 'password');
+		body.email = body.email.toLowerCase();
 
 
-db.sequelize.sync().then(function() {
+	db.user.authenticate(body).then(function (user){
+		res.json(user.toPublicJSON());
+	}, function () {
+		res.status(401).send();
+	});
+});
+
+
+
+
+db.sequelize.sync({force: true}).then(function() {
 	app.listen(PORT, function() {
 		console.log('Express listening on port ' + PORT + '!');
 	});
 
-})
+});
